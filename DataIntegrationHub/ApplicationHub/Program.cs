8d49034b630b17fb3cc5b3706987407708b54447
Joel Francisco <joeld.fran@hotmail.com>
2015-12-07 20:17:28 +0000

    Começo implementacao de ApplicationHub

---
+++ DataIntegrationHub/ApplicationHub/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationHub
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.Instance.Initialize();
+        }
+
+        static Program Instance = new Program();
+        private object _threadLock;
+        private MessageQueue _sensorMessageQueue;
+        public MessageQueue sensorMessageQueue
+        {
+            get { lock (_threadLock) { return _sensorMessageQueue; } }
+            set { lock (_threadLock) { _sensorMessageQueue = value; } }
+        }
+        private bool _isWorking;
+        public bool IsWorking {
+            get { lock (_threadLock) { return _isWorking; } }
+            set { lock (_threadLock) { _isWorking = value; } } 
+        }
+
+        private Program() { 
+            _threadLock = new object();
+            _sensorMessageQueue = MessageQueue.Create(".\\Private$\\newPrivQueue1");
+        }
+
+        public void Initialize()
+        {
+            IsWorking = true;
+        }
+
+
+        private void SensorDataListener()
+        {
+            while (IsWorking)
+            {
+
+            }
+        }
+    }
+}
---
+++ resources/db_1512071926.sql
@@ -0,0 +1,34 @@
+/*
+SQLyog Community v12.14 (64 bit)
+MySQL - 5.6.26-log : Database - is_dihub
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `record` */
+
+DROP TABLE IF EXISTS `record`;
+
+CREATE TABLE `record` (
+  `IdRecord` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Autoincrementing primary key',
+  `NodeId` int(10) NOT NULL COMMENT 'Id of the sensor',
+  `Channel` varchar(1) NOT NULL COMMENT 'Channel of the sensor',
+  `Value` float(6,4) NOT NULL DEFAULT '0.0000' COMMENT 'Value given by the sensor',
+  `DateCreated` datetime NOT NULL COMMENT 'Datetime of the record',
+  `DateCreatedTicks` bigint(20) NOT NULL COMMENT 'Datetime of the record represented by ammount of ticks',
+  PRIMARY KEY (`IdRecord`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `record` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
