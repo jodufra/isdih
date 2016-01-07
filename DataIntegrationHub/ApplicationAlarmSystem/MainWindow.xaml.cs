3cd01e2612866e836f9c88a4da9f7947715bf895
Mickael <mickaelgomes90@gmail.com>
2016-01-07 16:09:02 +0000

    Alarms files
    ficheiros alarms + xml

---
+++ DataIntegrationHub/ApplicationAlarmSystem/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Linq;
+using Microsoft.Win32;
+
+namespace ApplicationAlarmSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MyXmlHandler myxml = new MyXmlHandler("c:\\temp\\alarmsRules.xml","c:\\temp\\alarmsRules.xsd");
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            if (myxml.validateXml())
+                updateListView();
+            else
+            {
+                MessageBox.Show("O ficheiro xml Não é válido! O ficheiro XML foi criado!");
+                myxml.CreateXML();
+            }
+        }
+
+        
+
+        class DAL_OCUSMA
+        {
+       
+            public static List<Rule> LoadOCUSMA()
+            {
+                List<Rule> items = new List<Rule>();
+                var ruls = from c in XElement.Load("c:\\temp\\alarmsRules.xml").Elements("Rule") select c;
+                foreach (var rules in ruls)
+                {
+                    Rule lRule = new Rule
+                    {
+                        Channel = rules.Element("channel").Value,
+                        Min = int.Parse(rules.Element("min").Value),
+                        Max = int.Parse(rules.Element("max").Value)                     
+                    };
+                    items.Add(lRule);
+                    //Console.WriteLine(items[0].Channel);
+                }
+               
+                return items;
+            }
+
+        }
+
+       
+
+        public class Rule
+        {
+            public string Channel { get; set; }
+            public int Min { get; set; }
+            public int Max { get; set; }
+        }
+
+      
+
+        private void alarmsRules_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var selectedItem = (dynamic)alarmsRules.SelectedItems[0];
+            comboBoxChannel.SelectedValue = selectedItem.Channel;
+            txtMax.Text = Convert.ToString(selectedItem.Max);
+            txtMin.Text = Convert.ToString(selectedItem.Min);
+            //MessageBox.Show(selectedItem.Channel);
+        }
+
+
+        private void updateListView()
+        {
+            //Se o ficheiro nao existir, cria um ficheiro XML
+
+            //Carrega as lista para a listView
+            List<Rule> RuleList = new List<Rule>();
+            RuleList = DAL_OCUSMA.LoadOCUSMA();
+            alarmsRules.ItemsSource = RuleList;
+        }
+
+       
+
+        private void btnXML_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("c:\\temp\\alarmsRules.xml");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //myxml.CreateXML();
+            myxml.updateRules(comboBoxChannel.SelectedValue.ToString(), int.Parse(txtMin.Text), int.Parse(txtMax.Text));
+            updateListView();
+           
+        }  
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            myxml.deleteRules(comboBoxChannel.SelectedValue.ToString());
+            updateListView();
+        }
+
+    }
+}
---
+++ DataIntegrationHub/ApplicationAlarmSystem/MyXmlHandler.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace ApplicationAlarmSystem
+{
+    class MyXmlHandler{
+
+        private string _xmlfilepath;
+        private string _xsdfilepath;
+        private bool _isvalid = true;
+        private string _validationMessage;
+
+        public MyXmlHandler(string xmlfile, string xsdfile)
+        {
+            _xmlfilepath = xmlfile;
+            _xsdfilepath = xsdfile;
+        }
+
+        public void CreateXML()
+        {
+            //gerar por codigo um ficheiro XML
+            //alarmsRules.xml
+            XmlDocument doc = new XmlDocument();
+
+            //Create the XML declaration, and append it to XML document
+            XmlDeclaration dec = doc.CreateXmlDeclaration("1.0", "utf-8", null);
+            doc.AppendChild(dec);
+
+            //Create the root element
+            XmlElement root = doc.CreateElement("Rules");
+            doc.AppendChild(root);
+
+            //Create Rules
+            XmlElement rule = doc.CreateElement("Rule");    
+            rule.SetAttribute("type", "T");
+            rule.SetAttribute("min", "0");
+            rule.SetAttribute("max", "20");
+
+            root.AppendChild(rule);
+
+            doc.Save("c:\\temp\\alarmsRules.xml");
+        }
+        public void deleteRules(string channel)
+        {
+            XmlDocument doc1 = new XmlDocument();
+            doc1.Load(_xmlfilepath);
+            XmlNode _channelExist = doc1.SelectSingleNode("/Rules/Rule[channel='"+ channel +"']");
+            if (_channelExist != null)
+            {
+                // get its parent node
+                XmlNode parent = _channelExist.ParentNode;
+
+                // remove the child node
+                parent.RemoveChild(_channelExist);
+
+                // verify the new XML structure
+                string newXML = _channelExist.OuterXml;
+            }
+            doc1.Save(_xmlfilepath);
+        }
+
+        public void updateRules(string channel, int min, int max)
+        {
+            XmlDocument doc1 = new XmlDocument();
+            doc1.Load(_xmlfilepath);
+            //verificar se o channel existe
+            XmlNode _channelExist = doc1.SelectSingleNode("/Rules/Rule[channel='"+ channel +"']");
+            //para atualizar o channel, min e max
+            XmlNodeList _channelUpdate = doc1.SelectNodes("/Rules/Rule[channel='" + channel + "']");
+
+            if (_channelExist == null){
+                XmlElement newel = doc1.CreateElement("Rule");
+                XmlNode x = doc1.GetElementsByTagName("Rules")[0];
+                x.AppendChild(newel);
+
+                XmlElement _createChan = doc1.CreateElement("channel");
+                _createChan.InnerText = channel;
+                XmlElement _createMin = doc1.CreateElement("min");
+                _createMin.InnerText = Convert.ToString(min);
+                XmlElement _createMax = doc1.CreateElement("max");
+                _createMax.InnerText = Convert.ToString(max);
+                newel.AppendChild(_createChan);
+                newel.AppendChild(_createMin);
+                newel.AppendChild(_createMax);
+            }else{
+ 
+                foreach (XmlNode item in _channelUpdate)
+                {
+                    item["min"].InnerText = Convert.ToString(min);
+                    item["max"].InnerText = Convert.ToString(max);
+                }
+            }
+            doc1.Save(_xmlfilepath);
+        }
+
+
+        public bool validateXml()
+        {
+            _isvalid = true;
+            XmlDocument doc = new XmlDocument();
+            try
+            {
+                doc.Load(_xmlfilepath);
+                ValidationEventHandler eventHandler = new ValidationEventHandler(MyEvent);
+                doc.Schemas.Add(null, _xsdfilepath);
+                doc.Validate(eventHandler);
+            }
+            catch (XmlException ex)
+            {
+                _isvalid = false;
+                _validationMessage = string.Format("Error: {0}", ex.ToString()); // ex.Message
+            }
+            return _isvalid;
+        }
+
+        private void MyEvent(Object sender, ValidationEventArgs args)
+        {
+            _isvalid = false;
+
+            switch (args.Severity)
+            {
+                case XmlSeverityType.Error:
+                    _validationMessage = string.Format("Error: {0}", args.Message);
+                    break;
+                case XmlSeverityType.Warning:
+                    _validationMessage = string.Format("Warning: {0}", args.Message);
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+    }
+}
