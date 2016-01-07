e6792148825f43cd3a0a5648cfa226549c47e742
Rúben Santos <sam7.1@hotmail.com>
2016-01-07 23:19:10 +0000

    Melhoramento do start stop do hub; Praticamente concluido.

--- DataIntegrationHub/ApplicationHub/Application.cs
+++ DataIntegrationHub/ApplicationHub/Application.cs
@@ -19,15 +19,25 @@ namespace ApplicationHub
             // Global opts
             int Delay = Properties.Settings.Default.Delay, Port = Properties.Settings.Default.Port;
             string IpAddress = Properties.Settings.Default.IpAddress;
-            //bool Startup = Properties.Settings.Default.AutoStart;
+            string ZeroMqInitError = null;
 
             do
             {
                 if (Ignore==false)
-                    OpMM = MenuMain(CHController);
+                    OpMM = MenuMain(CHController, ZeroMqInitError);
                 switch (OpMM)
                 {
                     case 1:
+                        try
+                        {
+                            ZeroMQ.ZContext.Create();
+                        }
+                        catch (Exception e)
+                        {
+                            ZeroMqInitError = e.Message.ToString();
+                            break;
+                        }
+
                         if (CHController == null)
                         {
                             CHController = CommunicationHubController.CreateInstance();
@@ -94,7 +104,6 @@ namespace ApplicationHub
                                     {
                                         Console.Write("->Set Port number: ");
                                         convert2 = Int32.TryParse(Console.ReadLine(), out Port);
-                                        //fazer verificação
                                     } while (convert2 == false);
 
                                     //Save Setting
@@ -107,24 +116,6 @@ namespace ApplicationHub
                                     Ignore = true;
                                     OpSM = 0;
                                     break;
-                                /*case 4:
-                                    string temp;
-                                    do
-                                    {
-                                        Console.Write(Environment.NewLine + "->Set Auto Start On Startup (y/n): ");
-                                        temp = Console.ReadKey().KeyChar.ToString().ToLower();
-                                     
-                                        if (!temp.Equals("n") && !temp.Equals("y")) Console.WriteLine(Environment.NewLine + ">Value must be y or n!");
-                                    } while (!temp.Equals("n") && !temp.Equals("y"));
-
-                                    //Save Setting
-                                    Properties.Settings.Default.AutoStart = (temp.Equals("y"))? true : false;
-                                    Properties.Settings.Default.Save();
-                                    Startup = (temp.Equals("y")) ? true : false;
-
-                                    Ignore = true;
-                                    OpSM = 0;
-                                    break;*/
 
                             }
                         } while (OpSM != 0);
@@ -133,20 +124,23 @@ namespace ApplicationHub
                 }
             } while (OpMM != 0);
 
-            Environment.Exit(0); // To kill all thread in background correctly
+            Environment.Exit(0); // To kill all threads in background correctly
         }
 
-        public static int MenuMain(CommunicationHubController CHController)
+        public static int MenuMain(CommunicationHubController CHController, string zeroMqInitError)
         {
             int op;
             do
             {
-                
                 Console.Clear();
                 Console.WriteLine("------------- Hub App -------------");
                 Console.WriteLine("[1] " + (CHController == null ? "Start" : (CHController.IsSensorWorking() ? "Stop" : "Start")) + " Hub Data Recoding");
                 Console.WriteLine("[2] Settings");
                 Console.WriteLine("[0] Terminate");
+                if (!String.IsNullOrEmpty(zeroMqInitError))
+                {
+                    Console.WriteLine(zeroMqInitError + " The ZeroMQ dll's are missing, unable to start!");
+                }
                 Int32.TryParse(Console.ReadLine(), out op);
             } while (op != 1 && op != 2 && op != 0);
 
@@ -163,7 +157,6 @@ namespace ApplicationHub
                 Console.WriteLine("[1] Set Delay: " + (Delay == 0 ? "<Not Defined>" : Delay.ToString()));
                 Console.WriteLine("[2] Set Ip Address: " + (string.IsNullOrEmpty(Ip) ? "<Not Defined>" : Ip));
                 Console.WriteLine("[3] Set Port: " + (Port == 0 ? "<Not Defined>" : Port.ToString()));
-                //Console.WriteLine("[4] Set Auto Start On Startup: " + (Startup.Equals(' ') ? "<Not Defined>" : ((Startup) ? "Yes" : "No")));
                 Console.WriteLine("[0] exit");
                 Int32.TryParse(Console.ReadLine(), out op);
             } while (op != 1 && op != 2 && op != 3 && /*op != 4 &&*/ op != 0);
